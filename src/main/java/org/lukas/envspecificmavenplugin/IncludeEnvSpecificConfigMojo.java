3835d7ddeb2c1915c1d946dcef6fba285076dfaf
Martin Lukas <martinlukas24@protonmail.com>
2023-03-10 00:36:41 +0000

    Add plugin configuration

--- src/main/java/org/lukas/envspecificmavenplugin/IncludeEnvSpecificConfigMojo.java
+++ src/main/java/org/lukas/envspecificmavenplugin/IncludeEnvSpecificConfigMojo.java
@@ -2,13 +2,21 @@ package org.lukas.envspecificmavenplugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Copies only environment-specific configuration files into the final artifact.
  */
 @Mojo(name = "copy")
 public class IncludeEnvSpecificConfigMojo extends AbstractMojo {
+    @Parameter(property = "sourceDir", required = true)
+    private String sourceDir;
+    @Parameter(property = "env", required = true)
+    private String env;
+    
     public void execute() {
-        getLog().info("Hellooooooooo");
+        getLog().info(
+                "Source dir: '%s', env: '%s'".formatted(sourceDir, env)
+        );
     }
 }
