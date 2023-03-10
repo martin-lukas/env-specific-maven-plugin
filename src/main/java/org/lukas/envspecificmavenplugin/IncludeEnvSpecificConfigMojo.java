a299b1ed8d1cb26b083a69fa0330e2e579c3f6ae
Martin Lukas <martinlukas24@protonmail.com>
2023-03-10 00:38:34 +0000

    Add default lifecycle

--- src/main/java/org/lukas/envspecificmavenplugin/IncludeEnvSpecificConfigMojo.java
+++ src/main/java/org/lukas/envspecificmavenplugin/IncludeEnvSpecificConfigMojo.java
@@ -1,13 +1,14 @@
 package org.lukas.envspecificmavenplugin;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Copies only environment-specific configuration files into the final artifact.
  */
-@Mojo(name = "copy")
+@Mojo(name = "copy", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class IncludeEnvSpecificConfigMojo extends AbstractMojo {
     @Parameter(property = "sourceDir", required = true)
     private String sourceDir;
